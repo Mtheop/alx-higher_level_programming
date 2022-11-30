4b9336a7e03904b68efd4aab5c7a273a6bfac42c
Motheo <motheophetla@gmail.com>
2022-11-30 17:51:23 +0000

    Hot fix

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -21,6 +21,5 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
-def uppercase(str):
 
 #endif /* LISTS_H */
