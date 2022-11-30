b7224d5652489370e7b9031c394c416eb2c69e1c
Motheo <motheophetla@gmail.com>
2022-11-30 13:19:17 +0000

    FizzBuxx

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Print the numbers from 1 to 100 seperated by a space.
+    For multiples of three, print fixx insread of the number
+    For multiples of five, print buzz instead of the number.
+    For multiples of three and five, print Fizzbuzz instead of the number of the number.
+    """
+
+def fixbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 === 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
