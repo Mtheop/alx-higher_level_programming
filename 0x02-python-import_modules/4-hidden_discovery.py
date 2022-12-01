b0fabfdf62207d3cb83d777d391c61bb4eeda052
Motheo <motheophetla@gmail.com>
2022-12-01 19:38:45 +0000

    Hot fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 
-f __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for str in range(len(dir(hidden_4))):
+        if dir(hidden_4)[str][:2] != "__":
+            print("{:s}".format(dir(hidden_4)[str]))
