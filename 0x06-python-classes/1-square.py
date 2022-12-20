fd069dd6522b9aa40144a479555a3c96d6ba5db0
Motheo <motheophetla@gmail.com>
2022-12-20 13:09:21 +0000

    Hot fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,14 +6,14 @@ This module defines square by class.
 
 """
 
+
 class Square:
     """Represent a square."""
 
     def __init__(self, size):
-
         """Initialize a new Square.
+
         Args:
             size (int): The size of the new square.
-
         """
         self.__size = size
