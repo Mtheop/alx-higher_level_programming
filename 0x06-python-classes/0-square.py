f33c1e667f4c00afb9ca84f879a16ad92ea23071
Motheo <motheophetla@gmail.com>
2022-12-20 13:02:01 +0000

    Square by class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""Defines a class Square.
+
+This module defines the square of class.
+
+"""
 
 class Square:
     """Represents a square."""
+
     pass
