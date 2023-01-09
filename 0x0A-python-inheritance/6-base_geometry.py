4457b3c02514a9d36ac8a7491513098b2b07db01
Motheo <motheophetla@gmail.com>
2023-01-09 14:36:41 +0000

    Hot fix

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""Defines an inherited class-checking function."""
+"""Defines a base geometry class BaseGeometry."""
 
 
-def inherits_from(obj, a_class):
-    """Checks if an object is an inherited instance of a class.
+class BaseGeometry:
+    """Represent base geometry."""
 
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is an inherited instance of a_class - True.
-        Otherwise - False.
-    """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
